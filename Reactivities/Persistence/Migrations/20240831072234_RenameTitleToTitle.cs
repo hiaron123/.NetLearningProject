e93d8128f855a2f5843617b93bab26a35cf5648e
hiaron123 <kanaron12@gmail.com>
2024-09-01 22:52:38 +0800

    .NetLearning

---
+++ Reactivities/Persistence/Migrations/20240831072234_RenameTitleToTitle.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameTitleToTitle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Tittle",
+                table: "Activities",
+                newName: "Title");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Activities",
+                newName: "Tittle");
+        }
+    }
+}
